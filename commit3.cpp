86c11d43a68f9a36b5aa329da7fd439c0ef50d70
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-04-06 11:57:24 +0700

    add commit3.cpp with array access and modification examples

---
+++ commit3.cpp
@@ -0,0 +1,5 @@
+ //cara akses (membaca atau mengambil isi array)
+    cout << "Isi data kedua : " << dataku[1] << endl;
+    //mengisi array
+    dataku[1] = 200;
+    cout << "Isi data kedua setelah dirubah : " << dataku[1] << endl;
