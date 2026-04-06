27843b157dbf0ebb576c7df8e9e5869b5a046957
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-04-06 11:59:46 +0700

    add commit5.cpp to display data elements individually

---
+++ commit5.cpp
@@ -0,0 +1,6 @@
+//menampilkan data dengan perintah satu persatu
+    cout << "data pertama : " << datamu[0] << endl;
+    cout << "data kedua : " << datamu[1] << endl;
+    cout << "data ketiga : " << datamu[2] << endl;
+    cout << "data keempat : " << datamu[3] << endl;
+    cout << "data kelima : " << datamu[4] << endl;
