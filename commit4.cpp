dc2fc3bb8d528ea166d0e4154dfe62e91758406e
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-04-06 11:58:21 +0700

    add commit4.cpp with data input functionality for five elements

---
+++ commit4.cpp
@@ -0,0 +1,11 @@
+//mengisi data dengan perintah satu persatu
+    cout << "Isikan data pertama : ";
+    cin >> datamu[0];
+    cout << "Isikan data kedua : ";
+    cin >> datamu[1];
+    cout << "Isikan data ketiga : ";
+    cin >> datamu [2];
+    cout << "Isikan data keempat : ";
+    cin >> datamu[3];
+    cout << "Isikan data kelima : ";
+    cin >> datamu[4];
