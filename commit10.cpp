6cb1b47d2cf747e9bfcc69838e6539aa1011caea
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-04-06 12:09:00 +0700

    add commit10.cpp to implement data input for mahasiswa struct

---
+++ commit10.cpp
@@ -0,0 +1,9 @@
+//deklarasi variable struct
+    mahasiswa mhs;
+    //mengisi data
+    cout << "isikan data nama:";
+    cin >> mhs.nama;
+    cout << "isikan data alamat:";
+    cin >> mhs.alamat;
+    cout >> "isikan data usia:";
+    cin << mhs.umur;
